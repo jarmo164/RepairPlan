4956290e3880c2004147cc8df9d3012ec5c231b2
Koodikratt <koodikratt@local>
2026-03-31 12:32:57 +0300

    fix: use oscilloscope icon for electronics

--- static/js/ui.js
+++ static/js/ui.js
@@ -16,7 +16,7 @@
   };
 
   function electronicsIcon() {
-    return '<span class="electronics-icon" title="Elektrooniline"><svg viewBox="0 0 16 16" width="14" height="14" aria-hidden="true"><path fill="currentColor" d="M6 1h4v2h2v2h2v6h-2v2h-2v2H6v-2H4v-2H2V5h2V3h2V1Zm1 1v2h2V2H7ZM4 4v2H3v4h1v2h2v1h4v-1h2v-2h1V6h-1V4h-2V3H6v1H4Zm3 3h2v2H7V7Z"/></svg></span>';
+    return '<span class="electronics-icon" title="Elektrooniline parandaja"><svg viewBox="0 0 24 24" width="15" height="15" aria-hidden="true"><path fill="currentColor" d="M4 5.5A2.5 2.5 0 0 1 6.5 3h11A2.5 2.5 0 0 1 20 5.5v8a2.5 2.5 0 0 1-2.5 2.5h-11A2.5 2.5 0 0 1 4 13.5v-8Zm2.5-1A1.5 1.5 0 0 0 5 5.5v8A1.5 1.5 0 0 0 6.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 17.5 4.5h-11ZM6.5 18a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H7a.5.5 0 0 1-.5-.5Zm.766-8.142a.5.5 0 0 1 .702-.092l2.146 1.717 2.134-3.048a.5.5 0 0 1 .807.01l1.41 2.074 1.666-1.666a.5.5 0 1 1 .707.707l-2.09 2.09a.5.5 0 0 1-.777-.067l-1.289-1.897-2.091 2.987a.5.5 0 0 1-.729.105L7.358 10.56a.5.5 0 0 1-.092-.702Z"/></svg></span>';
   }
 
   function statusBadge(label) {
